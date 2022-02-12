82872cb6b3bc2d72b701d2c8e34460d85b1b5b25
ben.lam <ben-lam@outlook.com>
2022-02-12 14:49:47 +0800

    feat: update site

--- js/fastsearch.js
+++ js/fastsearch.js
@@ -2,10 +2,38 @@ var fuse; // holds our search engine
 var searchVisible = false; 
 var firstRun = true; // allow us to delay loading json data unless search activated
 var list = document.getElementById('searchResults'); // targets the <ul>
-var first = list.firstChild; // first child of search list
-var last = list.lastChild; // last child of search list
+var first = list.firstElementChild; // first child of search list
+var last = list.lastElementChild; // last child of search list
 var maininput = document.getElementById('searchInput'); // input box for search
 var resultsAvailable = false; // Did we get any search results?
+
+
+// search button
+document.querySelectorAll(".js-search").forEach(btn => {
+  console.debug('clicked')
+  btn.addEventListener('click', _triggerFastSearch, false);
+});
+
+function _triggerFastSearch(){
+  // Load json search index if first time invoking search
+  // Means we don't load json unless searches are going to happen; keep user payload small unless needed
+  if(firstRun) {
+    loadSearch(); // loads our json data and builds fuse.js search index
+    firstRun = false; // let's never do this again
+  }
+
+  // Toggle visibility of search box
+  if (!searchVisible) {
+    document.getElementById("fastSearch").style.visibility = "visible"; // show search box
+    document.getElementById("searchInput").focus(); // put focus in input box so you can just start typing
+    searchVisible = true; // search visible
+  }
+  else {
+    document.getElementById("fastSearch").style.visibility = "hidden"; // hide search box
+    document.activeElement.blur(); // remove focus from search box 
+    searchVisible = false; // search not visible
+  }
+}
 
 // ==========================================
 // The main keyboard event listener running the show
@@ -14,24 +42,7 @@ document.addEventListener('keydown', function(event) {
 
   // CMD-/ to show / hide Search
   if (event.metaKey && event.which === 191) {
-      // Load json search index if first time invoking search
-      // Means we don't load json unless searches are going to happen; keep user payload small unless needed
-      if(firstRun) {
-        loadSearch(); // loads our json data and builds fuse.js search index
-        firstRun = false; // let's never do this again
-      }
-
-      // Toggle visibility of search box
-      if (!searchVisible) {
-        document.getElementById("fastSearch").style.visibility = "visible"; // show search box
-        document.getElementById("searchInput").focus(); // put focus in input box so you can just start typing
-        searchVisible = true; // search visible
-      }
-      else {
-        document.getElementById("fastSearch").style.visibility = "hidden"; // hide search box
-        document.activeElement.blur(); // remove focus from search box 
-        searchVisible = false; // search not visible
-      }
+      _triggerFastSearch()
   }
 
   // Allow ESC (27) to close search box
@@ -46,8 +57,9 @@ document.addEventListener('keydown', function(event) {
   // DOWN (40) arrow
   if (event.keyCode == 40) {
     if (searchVisible && resultsAvailable) {
-      console.log("down");
       event.preventDefault(); // stop window from scrolling
+      first = list.firstElementChild;
+      last = list.lastElementChild;
       if ( document.activeElement == maininput) { first.focus(); } // if the currently focused element is the main input --> focus the first <li>
       else if ( document.activeElement == last ) { last.focus(); } // if we're at the bottom, stay there
       else { document.activeElement.parentElement.nextSibling.firstElementChild.focus(); } // otherwise select the next search result
@@ -58,6 +70,8 @@ document.addEventListener('keydown', function(event) {
   if (event.keyCode == 38) {
     if (searchVisible && resultsAvailable) {
       event.preventDefault(); // stop window from scrolling
+      first = list.firstElementChild;
+      last = list.lastElementChild;
       if ( document.activeElement == maininput) { maininput.focus(); } // If we're in the input box, do nothing
       else if ( document.activeElement == first) { maininput.focus(); } // If we're at the first item, go to input box
       else { document.activeElement.parentElement.previousSibling.firstElementChild.focus(); } // Otherwise, select the search result above the current active one
